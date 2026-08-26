228b0c8ccfa1c8765ea48db69daf35b05dfae8ee
NgoTuanTuns <ngotu6973@gmail.com>
2026-08-26 16:13:03 +0700

    Change duckdb to adls2 to utilize azure enviroment

--- dags/LoadDataToADLS.py
+++ dags/LoadDataToADLS.py
@@ -14,13 +14,12 @@ default_args = {
 def upload_data():
     try:
         hook = AzureDataLakeStorageV2Hook(adls_conn_id = 'adls2_connection')
-        print(os.listdir('/data'))
         for i in os.listdir('/data'):
             print(i)
             hook.upload_file_to_directory(
                 file_system_name = 'nyctaxidata',
                 directory_name = 'raw_data',
-                file_name = f'{i[0:i.find('_', i.find('_') + 1, len(i))]}.parquet',
+                file_name = i,
                 file_path = f'/data/{i}',
                 overwrite = True
             )
